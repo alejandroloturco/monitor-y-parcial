3d72690f7cc826e106e0575f2a1c81504112e4a1
Karensity12 <karen.agudelot@udea.edu.co>
2024-03-09 15:39:40 -0500

    Metodos get

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ class Implantes:
     def __init__(self):
         self.material = ""
         self.tamaño = 0
+        #Metodos get
+    def verMaterial(self):
+        return self.__material
+    def verTamaño(self):
+        return self.__tamaño
